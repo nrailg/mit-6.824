16faaf90eee3ba0ae260caf4abf06ba85d3ec983
nr <shibuyanorailgun@foxmail.com>
2019-12-08 21:02:10 +0800

    lab3 indent

--- src/kvraft/client.go
+++ src/kvraft/client.go
@@ -1,9 +1,10 @@
 package raftkv
 
-import "labrpc"
-import "crypto/rand"
-import "math/big"
-
+import (
+	"crypto/rand"
+	"labrpc"
+	"math/big"
+)
 
 type Clerk struct {
 	servers []*labrpc.ClientEnd
